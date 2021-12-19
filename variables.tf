1884a4928550fc68da1a1681d75a39c55422554f
tcjoel <tcjoel3@gmail.com>
2021-12-19 04:43:57 -0500

    terraform module

---
+++ variables.tf
@@ -0,0 +1,25 @@
+variable "region" {
+  default = "us-east-1"
+}
+
+variable "accesskey" {
+	default="" 
+}
+
+variable "secretkey" {
+	default=""
+}
+
+variable "vpcID" {
+	default="vpc-da11a7a7"
+}
+variable "amiID" {
+	default="ami-0b0af3577fe5e3532"
+}
+variable "keyPair" {
+	default="jenkins"
+}
+
+
+
+
